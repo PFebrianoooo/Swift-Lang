767b95e8b0619d8fe7a0cb3dbb611b3bb34ce5fe
Putra Pebriano <putrapebriano27@gmail.com>
2023-09-23 02:12:43 +0700

    first commit to learn swift

---
+++ Basic swift/Syntax/Exercise 2/Exerice 2.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+// car
+// mutable
+var Model = "corolla"
+var PaintColor = "red"
+var Mileage = 205.80
+var IsManualTransmision = true
+
+print(Model)
+print(Mileage)
+
+Model = "Fortuner"
+Mileage = 230.80
+print(Model)
+print(Mileage)
+
+// imutable
+let Make = "By Honda"
+let Year = 2000
+let Seat = 5
+
+
+print(Model)
+print(PaintColor)
+print(Make)
+print(Year)
+print(Mileage)
+print(IsManualTransmision)
+print(Seat)
+
+
+

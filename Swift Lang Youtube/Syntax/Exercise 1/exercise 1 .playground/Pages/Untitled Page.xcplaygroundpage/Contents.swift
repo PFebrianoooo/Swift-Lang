f84ef4af665e7f47e21395fe78a60648de64ba0d
PFebrianoooo <putrapebriano27@gmail.com>
2023-11-29 20:51:08 +0700

    add old material files

---
+++ Swift Lang Youtube/Syntax/Exercise 1/exercise 1 .playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+/*:
+ ### Example
+ */
+var str = "Hello, playground"
+
+var message = "Hello, World!"
+print(message)
+
+message = "My Message!"
+print(message)
+
+var String = "My exercise 1"
+
+var Name = "Putra Pebriano Nurba"
+print(Name)
+
+Name = "also called Edoy or Putra"
+print(Name)
+
+
+
+
+/*:
+ ### Exercise
+ 
+ 1.  Create a new variable ( var ) that stores your name
+ 2.  Print the variable
+ */
